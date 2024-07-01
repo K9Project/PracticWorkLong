797d885a1eef909ecfa07cc411e744530096148f
K9Project <v.kh1992@mail.ru>
2024-07-01 14:20:26 +0400

    Code Added

---
+++ PracticWorkLong.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+
+int main()
+{
+    std::string practic = "Okay";
+    std::cout << practic << "\n";
+    std::cout << practic.length() << "\n";
+    std::cout << practic[0] << "\n";
+    std::cout << practic[practic.size() - 1] << "\n";
+}
